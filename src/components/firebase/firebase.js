774982965fd94bf22025f4366bb6275904cf64a4
Abhishek kalra <55233688+Abhishek07Kalra@users.noreply.github.com>
2021-12-17 10:24:46 +0530

    Update firebase.js

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -2,12 +2,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import 'firebase/compat/firestore';
 var firebaseConfig = {
-    apiKey: "AIzaSyD_7sJAaAMb_Q5CJ3BGFNTYS5WfRnpDLRY",
-  authDomain: "easypoll-41ec0.firebaseapp.com",
-  projectId: "easypoll-41ec0",
-  storageBucket: "easypoll-41ec0.appspot.com",
-  messagingSenderId: "224360787511",
-  appId: "1:224360787511:web:9d95065dd8c5316ed159b2"
+    
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
